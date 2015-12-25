69aa8b5a2038033efbb6cf1935fe86919ab44ee3
Silver <silverprize@outlook.com>
2015-12-25 18:48:40 +0900

    fix css - apply responsive layout to thumbnail of right sidenav - apply scrollable to content area of sidenav

--- app/drive/driveDirectives.js
+++ app/drive/driveDirectives.js
@@ -4,7 +4,7 @@
   angular.module('materialDrive')
   .directive('mtdRightClick', ['$parse', '$rootScope', MtdRightClick])
   .directive('mtdFocus', ['$timeout', MtdFocus])
-  .directive('mtdDetails', ['mimeType', MtdDetails]);
+  .directive('mtdDetails', [MtdDetails]);
 
   function MtdRightClick($parse, $rootScope) {
     return {
@@ -40,26 +40,19 @@
     };
   }
 
-  function MtdDetails(mimeType) {
+  function MtdDetails() {
     return {
+      restrict: 'EA',
       scope: {
-        item: '=mtdDetails'
+        item: '=mtdDetailsItem'
       },
       templateUrl: 'app/drive/details.tpl.html',
       controller: ['$scope', '$mdSidenav', '$mdMedia', '$cacheFactory',
            function($scope,   $mdSidenav,   $mdMedia,   $cacheFactory) {
-        var self = this;
-
-        self.isFolder = isFolder;
-
         if (!!($mdMedia('gt-md') && $cacheFactory.get('details').get('visible'))) {
           $mdSidenav('details').open();
         } else {
           $mdSidenav('details').close();
-        }
-
-        function isFolder() {
-          return $scope.item && $scope.item.mimeType === mimeType.folder;
         }
       }],
       controllerAs: 'detailsCtrl'
